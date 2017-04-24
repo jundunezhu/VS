4abc59ff79a95f7cf069261be07827490c41754b
jundunezhu <junyuzhu000@gmail.com>
2017-04-23 17:20:04 -0700

    Sunday 4-23

---
+++ ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeConditionalRadioButton.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Your Note Taking Preferences<br />
+        <br />
+        <asp:RadioButton ID="RadioButton1" runat="server" GroupName="Notes" Text="Pencil" />
+        <br />
+        <asp:RadioButton ID="RadioButton2" runat="server" GroupName="Notes" Text="Pen" />
+        <br />
+        <asp:RadioButton ID="RadioButton3" runat="server" GroupName="Notes" Text="Phone" />
+        <br />
+        <asp:RadioButton ID="RadioButton4" runat="server" GroupName="Notes" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+        <br />
+        <asp:Image ID="Image1" runat="server" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Type"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton/ChallengeConditionalRadioButton/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            if (RadioButton1.Checked)
+                Label1.Text = "Pencil";
+            else if (RadioButton2.Checked)
+                Label1.Text = "Pen";
+            else if (RadioButton3.Checked)
+                Label1.Text = "Phone";
+            else if (RadioButton4.Checked)
+                Label1.Text = "Tablet";
+            else
+                Label1.Text = "Please select";     
+            //Both texted and image will be displayed.
+            if (RadioButton1.Checked)
+                Image1.ImageUrl = "pencil.png";
+            else if (RadioButton2.Checked)
+                Image1.ImageUrl = "pen.png";
+            else if (RadioButton3.Checked)
+                Image1.ImageUrl = "phone.png";
+            else if (RadioButton4.Checked)
+                Image1.ImageUrl = "tablet.png";
+            else
+                Label1.Text = "Please select";
+
+        }
+    }
+}
