91d4d3d52fc3da46848287dc04859784210d2f7b
jundunezhu <junyuzhu000@gmail.com>
2017-04-23 17:35:09 -0700

    Sunday 4-23.

---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void additionButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstValueTextBox.Text);
+            int second = int.Parse(secondValueTextBox.Text);
+            int result = first + second;
+            resultLabel.Text = result.ToString();      
+        }
+
+        protected void subtractionButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstValueTextBox.Text);
+            int second = int.Parse(secondValueTextBox.Text);
+            int result = first - second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(firstValueTextBox.Text);
+            int second = int.Parse(secondValueTextBox.Text);
+            int result = first * second;
+            resultLabel.Text = result.ToString();
+        }
+
+        protected void divisionButton_Click(object sender, EventArgs e)
+        {
+            double first = double.Parse(firstValueTextBox.Text);
+            double second = double.Parse(secondValueTextBox.Text);
+            double result = first / second;
+            resultLabel.Text = result.ToString();
+        }
+    }
+}
